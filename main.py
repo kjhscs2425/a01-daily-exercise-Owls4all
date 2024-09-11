3de17b30265650b83e89c73803ad37ec07054a06
owls4all <owls4all@gmail.com>
2024-09-11 12:06:21 -0700

    Drew hexagon

--- main.py
+++ main.py
@@ -1,2 +1,24 @@
 import turtle
-
+#
+#
+#begin
+t = turtle
+t.speed(10)
+side = 250
+theta = 120
+phi = 60
+t.forward(side)
+t.left(theta)
+t.forward(side)
+for i in range(3):
+    t.left(theta)
+    t.forward(2*side)
+    t.left(theta)
+    t.forward(side)
+for i in range(6):
+    t.left(phi)
+    t.forward(side)
+#end
+#
+#
+turtle.exitonclick()
